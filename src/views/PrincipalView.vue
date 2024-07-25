807c6c31efc7ec18396299b3ffb3695ef6562947
yarlingj27 <yarlingj27@gmail.com>
2024-07-25 18:13:13 -0500

    Agrego imagenes y creo base de datos

--- src/views/PrincipalView.vue
+++ src/views/PrincipalView.vue
@@ -1,6 +1,32 @@
-
 <template>
-  <article class="contenedor">
-    pagina principal
+  <article id="contenedor">
+    <section id="seccion4">
+      <article>
+        <h2>
+          ¡¡Amigurumis!! Adorables muñecos personalizados,llaveros, ramos
+          tejidos con amor a mano en hilo crochet.
+        </h2>
+      </article>
+    </section>
+
+    <section id="seccion3">
+      <article>
+        <img src="/src/assets/imagenes/1.jpg" alt="" width="300" height="200" />
+        <h3>Amigurumis</h3>
+      </article>
+      <article>
+        <img src="/src/assets/imagenes/5.jpg" alt="" width="300" height="200" />
+        <h3>Llaveros</h3>
+      </article>
+      <article>
+        <img
+          src="/src/assets/imagenes/10.jpg"
+          alt=""
+          width="300"
+          height="200"
+        />
+        <h3>Ramos</h3>
+      </article>
+    </section>
   </article>
 </template>
