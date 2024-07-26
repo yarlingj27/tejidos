c01528a937f40890b55996c8564dc9b6c8dc8176
yarlingj27 <yarlingj27@gmail.com>
2024-07-25 21:07:51 -0500

    Creo pagina de contacto

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,6 +1,36 @@
-
 <template>
- <article class="contenedor">
-    pagina contacto
-  </article>
+  <section id="contacto">
+    <section id="contenedorContacto">
+      <article id="imagenContacto">
+        <img src="/src/assets/imagenes/foto.jpeg" alt="" width="500" />
+      </article>
+      <article id="informacion">
+        <h1>Tejidos Yarlin</h1>
+        <p><b>Dirección:</b> Calle 19a # 32 - 20</p>
+        <p><b>Teléfono:</b> 3116163373</p>
+        <p><b>Correo electrónico:</b> yarlingj27@gmail.com</p>
+        <p><b>Horario:</b> Lunes a Viernes, 9:00 AM - 6:00 PM</p>
+
+        <section id="redesMetodo">
+        <section id="columna">
+          <h2>Redes Sociales</h2>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">TikTok</a></li>
+          </ul>
+        </section>
+        <section id="columna">
+          <h2>Métodos de pago</h2>
+          <ul>
+            <li>Visa</li>
+            <li>Mastercard</li>
+            <li>PayPal</li>
+          </ul>
+        </section>
+      </section>
+      </article>
+      
+    </section>
+  </section>
 </template>
